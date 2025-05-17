dbaf41bcfec2951f5a4e2406141d3c7a84ab0a41
divagueame <martin.arce.teixeira@gmail.com>
2025-05-17 14:05:13 +0200

    Add mission control

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "products#index"
   get "/refresh", to: "suggestions#refresh"
+  mount MissionControl::Jobs::Engine, at: "/jobs"
 end
